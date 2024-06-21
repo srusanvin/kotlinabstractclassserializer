6375b6288094ec952d5a0f6bc473c9bf54a50958
srusanvin <srusanvin@gmail.com>
2024-06-21 21:28:03 +0100

    root node type info not serialized sample

---
+++ kotlinpolydeserializer/src/main/kotlin/Programme.kt
@@ -0,0 +1,9 @@
+abstract class Programme {
+    val synopsis: List<String>
+        get() = resolveData()
+
+    private fun resolveData(): List<String> {
+        return arrayListOf("my synopsis1")
+    }
+}
+data class Movie(val lastInSeason: Boolean? = null): Programme() {}
